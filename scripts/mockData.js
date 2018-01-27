3f1fc950b3cfd7493f88de6c22d1b158c4ef78b3
Zhou <540376482yzb@gmail.com>
2018-01-27 14:04:34 -0800

    uploading project to thinkful-org

---
+++ scripts/mockData.js
@@ -0,0 +1,25 @@
+'use strict'
+const mockData = function (){
+  return{
+    items:[    
+      {
+        'title': 'Google',
+        'url': 'https://google.com',
+        'desc': 'An indie search engine startup An indie search engine startup An indie search engine startup An indie search engine startup An indie search engine startup',
+        'rating': 4
+      },
+      {
+        'title': 'Facebook',
+        'url': 'https://facebook.com',
+        'desc': 'An indie search engine startup An indie search engine startup An indie search engine startup An indie search engine startup An indie search engine startup',
+        'rating': 3
+      },
+      {
+        'title': 'Twitter',
+        'url': 'https://twitter.com',
+        'desc': 'An indie search engine startup An indie search engine startup An indie search engine startup An indie search engine startup An indie search engine startup',
+        'rating': 2
+      }
+    ]
+  }
+}()
